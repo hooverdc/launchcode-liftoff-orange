5fed915ab8229e71645efbf5a2be3ec7557ce131
briannaclarkso <brianna.extra@gmail.com>
2024-03-07 21:10:40 -0600

    Project Creation

---
+++ demo/src/test/java/com/Launchcode/demo/NationalParkAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Launchcode.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NationalParkAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
