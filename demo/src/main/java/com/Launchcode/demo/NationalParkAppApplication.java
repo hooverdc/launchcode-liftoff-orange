50adc7d605f7168074c9f3a020115c573ce0040b
briannaclarkso <brianna.extra@gmail.com>
2024-03-10 22:06:41 -0500

    New branch commit

---
+++ demo/src/main/java/com/Launchcode/demo/NationalParkAppApplication.java
@@ -0,0 +1,13 @@
+package com.Launchcode.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NationalParkAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NationalParkAppApplication.class, args);
+	}
+
+}
