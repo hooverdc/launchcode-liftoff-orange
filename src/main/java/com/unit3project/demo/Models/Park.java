44cdac3694253176c54ff09a05abac2ee2ee1327
eschremp <eschremp93@gmail.com>
2024-03-13 17:55:05 -0500

    created intial objects

---
+++ src/main/java/com/unit3project/demo/Models/Park.java
@@ -0,0 +1,8 @@
+package com.unit3project.demo.Models;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Park extends AbstractEntity{
+    //needs properties, getters/setters, many-to-many w/ user,
+}
---
+++ src/main/java/com/unit3project/demo/Models/Review.java
@@ -0,0 +1,8 @@
+package com.unit3project.demo.Models;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Review extends AbstractEntity{
+    //properties, getters/setters, many-to-many w/park, many-to-many w/ user
+}
