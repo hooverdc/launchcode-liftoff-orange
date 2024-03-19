6dbf46df7356558c0c49704b9235d1c1e7de30d4
briannaclarkso <brianna.extra@gmail.com>
2024-03-19 18:23:57 -0500

    ignore others did this

--- src/main/java/com/unit3project/demo/Models/Itinerary.java
+++ src/main/java/com/unit3project/demo/Models/Itinerary.java
@@ -4,6 +4,6 @@ package com.unit3project.demo.Models;
 import jakarta.persistence.Entity;
 
 @Entity
-public class Itinerary extends Park{
+public class Itinerary extends AbstractEntity{
     //properties, getters/setters, how many itineraries can each user have?
 }
