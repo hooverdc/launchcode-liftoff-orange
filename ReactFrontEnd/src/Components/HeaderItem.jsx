23ee63ec0d1fac1433391f06d0ad01988baf4f9a
Kevin Wiegman <wiegmank@gmail.com>
2024-04-01 20:56:43 -0500

    added comments re headerItem

--- ReactFrontEnd/src/Components/HeaderItem.jsx
+++ ReactFrontEnd/src/Components/HeaderItem.jsx
@@ -4,17 +4,10 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 {/* this functional component generates each nav link from the "menu" const in 
-Header.jsx into its own element in the Header */}
+Header.jsx into its own element in the Header. Using <Link> below with the <Outlet> in
+the Header component reduces the amount of components that are rerendered. */}
 function HeaderItem({name,Icon,path}) {
   return (
-    // <div className="text-white flex items-center gap-3 
-    // text-[18px] font-semibold cursor-pointer hover:underline underline-offset-8">
-    //     <Icon/>
-
-    //     {/* this will hide the text for each nav link in the header when the 
-    //     screen width is small/mobile */}
-    //     <h2 className="hidden md:block">{name}</h2>
-    // </div>
     <>
         <Link to={path}>
           <div className="text-white flex items-center gap-3 
