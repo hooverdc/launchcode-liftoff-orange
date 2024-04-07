90e7f3955701f7b3ab3da5283d25de4dd5efe4cc
wiegmank <78716591+wiegmank@users.noreply.github.com>
2024-04-07 17:46:17 -0500

    park search now linked to park details

--- ReactFrontEnd/src/Components/Itinerary.jsx
+++ ReactFrontEnd/src/Components/Itinerary.jsx
@@ -2,13 +2,21 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Slideshow from './Slideshow'
 import Header from './Header'
+import { queryClient } from '../main'
 
 function Itinerary() {
+
+  // const data = queryClient.getQueryData(['someparks'])
+  // const parksFound = queryClient.getQueryData(['parksFound'])
+  
   return (
     <>
         <Header />
-        <Slideshow />
+        {/* <Slideshow /> */}
         <h1>THIS IS THE ITINERARY PAGE</h1>
+        {/* {console.log("IMPORT PARKS INFO")}
+        {console.log(data)}
+        {console.log(parksFound)} */}
     </>
   )
 }
