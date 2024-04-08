a5d867316ac34d146f6a968bfc60451f71167ce9
carnasci <cen.master24@gmail.com>
2024-04-07 19:47:40 -0500

    debugging itinerary

---
+++ ReactFrontEnd/src/Components/Iteneray.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Iteneray = () => {
+  return (
+    <>
+        <div>
+            <div>
+                <h1>Create Itenerary for ?</h1>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Iteneray
