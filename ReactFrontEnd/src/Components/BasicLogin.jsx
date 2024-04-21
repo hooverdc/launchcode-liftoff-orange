bd968baada9fc476dc80d0aa406d6cd65a86b3c6
wiegmank <78716591+wiegmank@users.noreply.github.com>
2024-04-21 17:51:52 -0500

    testing basic login component

--- ReactFrontEnd/src/Components/BasicLogin.jsx
+++ ReactFrontEnd/src/Components/BasicLogin.jsx
@@ -1,32 +1,42 @@
+import axios from 'axios';
 import React, {useState} from 'react'
 
 function BasicLogin() {
 
-    const [ username, setUsername ] = useState([""]);
-    const [ password, setPassword ] = useState([""]);
-    const [ user, setUser ] = useState([]);
+  const [ username, setUsername ] = useState([""]);
+  const [ password, setPassword ] = useState([""]);
+  const [ user, setUser ] = useState();
 
+  const handleSubmit = async event => {
+    event.preventDefault();
+    const user = { username, password };
+    const response = await axios.post(
+      "http://localhost:8080/login", user
+      );
+    setUser(response.data)
+    localStorage.setItem('user', response.data)
+    console.log(response.data)
+  };
 
-
-
-
-
-
-
+  if (user) {
+    return <div>{user.username} is logged in</div>
+  }
 
 
   return (
     <>
-
-        <div className="flex flex-col items-center border border-nps-green-999">
+      <form onSubmit={handleSubmit}>
+        <div className="flex flex-col items-center border border-nps-green-999 w-1/2 mx-auto rounded-xl">
             <div className="underline underline-offset-2">LOGIN</div>
-            <label>Username: <input type="text" /></label>
+            <label>Username: <input type="text" placeholder="enter your username..." value={username} 
+            onChange={({ target }) => setUsername(target.value)}/></label>
             
-            <label>Password: <input type="password" /></label>
+            <label>Password: <input type="password" placeholder="enter your password..." value={password}
+            onChange={({ target }) => setPassword(target.value)}/></label>
             
-            <button type="submit">Login</button>
+            <button type="submit" className="bg-nps-green-700 m-4">Login</button>
         </div>
-    
+      </form>
     </>
     
   )
