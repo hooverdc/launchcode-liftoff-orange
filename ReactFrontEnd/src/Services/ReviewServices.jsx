1d3a512631809792912579499d027c4155306b37
carnasci <cen.master24@gmail.com>
2024-04-23 17:02:03 -0500

    review services and relational mapping added

--- ReactFrontEnd/src/Services/ReviewServices.jsx
+++ ReactFrontEnd/src/Services/ReviewServices.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import axios from 'axios'
 
-const REVIEW_API_BASE_URL = "http://localhost:8080/api/v1/createreview";
+const REVIEW_API_BASE_URL = "http://localhost:8080/api/v1/review";
 
 class ReviewService {
-    createReview(review) {
-        return axios.post(REVIEW_API_BASE_URL, review)
-    }
+  createReview(review) {
+    return axios.post(REVIEW_API_BASE_URL, review);
+  }
+
+  getReviewsByUserId(id) {
+    return axios.get(REVIEW_API_BASE_URL + "/" + "user", id);
+  }
+
+  getReviewsByParkcode(parkcode) {
+    return axios.get(REVIEW_API_BASE_URL + "/" + "parkcode" + parkcode);
+  }
+
+  deleteReview(id) {
+    return axios.delete(REVIEW_API_BASE_URL + "/" + id);
+  }
 }
 
 export default new ReviewService();
--- SpringBootBackEnd/demo/src/main/java/com/nationalParkApp/demo/Model/Review.java
+++ SpringBootBackEnd/demo/src/main/java/com/nationalParkApp/demo/Model/Review.java
@@ -1,5 +1,6 @@
 package com.nationalParkApp.demo.Model;
 
+import com.nationalParkApp.demo.entity.UserEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,4 +12,11 @@ public class Review {
     private long id;
     private String parkCode;
     private String content;
+    private UserEntity user;
+
+    public Review(long id, String parkCode, String content) {
+        this.id = id;
+        this.parkCode = parkCode;
+        this.content = content;
+    }
 }
